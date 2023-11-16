7a1f7bc9f6ee3ea2a0207e91cc1444ed68469b40
Carson Gross <carsongro@gmail.com>
2023-11-15 19:04:24 -0700

    Updated tests and license text in files

--- Tests/IoImageTests/ImageLoaderTests.swift
+++ Tests/IoImageTests/ImageLoaderTests.swift
@@ -1,17 +1,38 @@
+//
+//  ImageLoaderTests.swift
+//
+//
+//  Created by Carson Gross on 11/15/23.
+//
+//  Copyright (c) 2023 Carson Gross
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import SwiftUI
 import XCTest
 @testable import IoImage
 
 let testURL = URL(string: "https://github.com/carsongro/IoImage/blob/main/Tests/IoImageTests/clouds.jpeg?raw=true")!
 
 final class ImageLoaderTests: XCTestCase {
-    func testDownloadImage() {
-        Task {
-            do {
-                let image = try await IoImageLoader.shared.Image(from: testURL)
-                XCTAssertNotNil(image)
-            } catch {
-                XCTAssertTrue(false)
-            }
-        }
+    func testDownloadImage() async throws {
+        let image = try await IoImageLoader.shared.Image(from: testURL)
+        let _ = try XCTUnwrap(image as Image, "Expected a SwiftUI Image.")
     }
 }
